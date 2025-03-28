f20dfdbf8e186016cec4e2add58c86564ba644b9
Saban yilmaz <syilmaz002@ensc.fr>
2025-03-29 00:14:35 +0100

    Update middleware to correct the route matcher for the "/standing" route, ensuring proper Clerk authentication for protected routes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/home(.*)", "/standings(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/home(.*)", "/standing(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
