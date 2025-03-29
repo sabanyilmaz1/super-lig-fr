067d4012f8c254d8ec1837c5a096ccc1ddfce3c6
Saban yilmaz <syilmaz002@ensc.fr>
2025-03-29 20:59:17 +0100

    Add DisplayArticlesHome component and mock articles data for blog section, enhancing article display functionality on the homepage.

---
+++ src/components/blog/display-articles-home.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+
+export const DisplayArticlesHome = () => {
+  return (
+    <Card className="border-2 border-redsuperlig bg-gradient-to-r from-red-700 to-red-500 shadow-lg min-h-[400px]">
+      <CardHeader></CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-1 gap-3  lg:grid-cols-[70%_30%]">
+          {/* Article Principal */}
+          <Card className="overflow-hidden shadow-none  bg-transparent !border-none text-white">
+            <CardContent className="space-y-4 !p-0">
+              <div className="relative w-[95%]  rounded-lg overflow-hidden">
+                <Image
+                  src="/blog/article1.jpg"
+                  alt="Article 1"
+                  fill
+                  className="object-cover transition-transform duration-300 hover:scale-110"
+                />
+              </div>
+              <div className="space-y-4 md:px-6">
+                {/* <h2 className="text-lg font-bold md:text-xl ">
+                  <Link href={"#"}>{articles[0].title}</Link>
+                </h2> */}
+                <div className="text-sm italic">Publié le 13 octobre 2024</div>
+                {/* <article
+                  className="text-sm line-clamp-4 md:line-clamp-3 md:text-base"
+                  dangerouslySetInnerHTML={{ __html: articles[0].content }}
+                /> */}
+                <Link href={"#"} className="inline-block hover:underline">
+                  Lire la suite →
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+
+          {/* Articles Récents */}
+          <div className="space-y-2">
+            <div className="grid grid-cols-2 gap-3 md:grid-cols-1">
+              {/* {articles
+                .filter((_, index) => index !== 0)
+                .map((article, index) => (
+                  <Card
+                    key={article.id}
+                    className="p-0 overflow-hidden text-white transition-colors bg-transparent border-none shadow-none"
+                  >
+                    <CardContent className="!p-0">
+                      <div className="flex flex-col gap-2">
+                        <div className="relative flex-shrink-0 w-full overflow-hidden">
+                          <Image
+                            src={
+                              index === 1
+                                ? "/blog/article2.jpg"
+                                : index === 2
+                                ? "/blog/article3.jpg"
+                                : "/blog/article4.jpg"
+                            }
+                            alt={article.title}
+                            fill
+                            className="object-cover transition-transform duration-300 rounded-md hover:scale-110 md:w-full md:h-auto w-[140px] h-[80px] "
+                          />
+                        </div>
+                        <h3 className="text-xs font-semibold ">
+                          <Link href={`#`}>{article.title}</Link>
+                        </h3>
+                      </div>
+                    </CardContent>
+                  </Card>
+                ))} */}
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ src/components/blog/mock.ts
@@ -0,0 +1,62 @@
+export const articles = [
+  {
+    _id: "jd75zpw72swqrdswg9ackbghw97cmy6k",
+    authorId: "j577g714ng6j5c677h3khvsvv17cnfpf",
+    content: `# Galatasaray : Une domination incontestée
+  
+  **Galatasaray** continue de dominer la Süper Lig turque cette saison. Après 27 journées, l'équipe stambouliote totalise 71 points, avec 22 victoires et 5 nuls, restant invaincue jusqu'à présent. Ils possèdent une avance de 9 points sur leur rival, **Fenerbahçe**, qui compte 62 points en 26 matchs.
+  
+  ## Une attaque prolifique
+  
+  Le succès du Galatasaray est en grande partie attribuable à son attaque redoutable. **Victor Osimhen**, l'attaquant nigérian prêté par Naples, est le meilleur buteur du championnat avec 20 buts en 22 matchs, affichant une moyenne impressionnante de 0,91 but par match. Sa complémentarité avec l'Espagnol **Álvaro Morata**, auteur de performances notables, renforce l'efficacité offensive de l'équipe.
+  
+  ## Prochain défi
+  
+  Le prochain défi du Galatasaray sera contre **Samsunspor**, actuellement troisième avec 51 points. Une victoire consoliderait davantage leur position de leader et les rapprocherait du titre national.`,
+    title: "Galatasaray maintient son avance en tête de la Süper Lig",
+  },
+  {
+    _creationTime: 1742746648438.606,
+    _id: "jd78bpwr2ehn6hmw1t3jwqz1z97cnpa5",
+    authorId: "j577g714ng6j5c677h3khvsvv17cnfpf",
+    content: `# Fenerbahçe : Lutte acharnée pour le titre
+  
+  Sous la direction de **José Mourinho**, Fenerbahçe occupe la deuxième place du championnat avec 62 points en 26 matchs. Malgré une attaque efficace menée par **Youssef En-Nesyri**, auteur de 16 buts cette saison, l'équipe peine à rattraper Galatasaray.
+  
+  ## Récents déboires
+  
+  Le récent match nul 0-0 contre **Samsunspor** a freiné leur progression. Pour rester dans la course au titre, Fenerbahçe devra impérativement retrouver le chemin de la victoire lors des prochaines rencontres.`,
+    title: "Fenerbahçe à la poursuite du leader",
+  },
+  {
+    _creationTime: 1742746648438.6062,
+    _id: "jd70vnrpapwy3fs5g3x45cawe57cm04c",
+    authorId: "j577g714ng6j5c677h3khvsvv17cnfpf",
+    content: `# Samsunspor : La révélation inattendue
+  
+  Promu cette saison, **Samsunspor** crée la surprise en Süper Lig en se hissant à la troisième place avec 51 points en 27 matchs. Leur solidité défensive et leur efficacité offensive leur ont permis de rivaliser avec les cadors du championnat.
+  
+  ## Un test décisif à venir
+  
+  Leur prochain match contre le leader, **Galatasaray**, sera un véritable test de leur ambition. Une performance solide pourrait confirmer leur statut de prétendant sérieux aux places européennes.`,
+    title: "Samsunspor, la surprise de la saison",
+  },
+  {
+    _creationTime: 1742746648438.6064,
+    _id: "jd78ckn4qa856k7w1093mb0g617cm5hh",
+    authorId: "j577g714ng6j5c677h3khvsvv17cnfpf",
+    content: `# Controverse sur le terrain
+  
+  Le 9 février 2025, une controverse a éclaté lors du match entre **Galatasaray** et **Adana Demirspor**. Après l'attribution d'un penalty contesté en faveur de Galatasaray, transformé par **Álvaro Morata** à la 12e minute, l'équipe d'Adana Demirspor a quitté le terrain en signe de protestation, estimant que la décision arbitrale était injuste. Cette action a conduit à l'arrêt du match. La Fédération turque de football a depuis attribué une victoire 3-0 à Galatasaray et infligé une pénalité de trois points à Adana Demirspor. Cet incident a suscité de nombreuses réactions dans le monde du football turc, certains appelant à une réforme du système d'arbitrage.`,
+    title: "Polémique lors du match Galatasaray - Adana Demirspor",
+  },
+  {
+    _creationTime: 1742746648438.6067,
+    _id: "jd78v3dnbnzc8txd9nztyh5rbn7cmmvs",
+    authorId: "j577g714ng6j5c677h3khvsvv17cnfpf",
+    content: `# Osimhen : L'homme providentiel
+  
+  Le 15 mars 2025, **Galatasaray** a consolidé sa position de leader en Süper Lig en s'imposant 2-1 contre **Çaykur Rizespor**. L'attaquant nigérian **Victor Osimhen** a été le héros du match en inscrivant deux buts, portant son total à 19 réalisations cette saison. Cette performance souligne l'importance d'Osimhen dans l'effectif de Galatasaray, surtout en l'absence d'**Álvaro Morata**, blessé. Grâce à cette victoire, Galatasaray maintient une avance de six points sur Fenerbahçe, son dauphin.`,
+    title: "Victor Osimhen maintient Galatasaray en tête",
+  },
+];
