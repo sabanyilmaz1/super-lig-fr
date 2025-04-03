05a22f45a416b1f3cc7006d28fad8db0376b39f4
Saban yilmaz <syilmaz002@ensc.fr>
2025-04-03 22:02:20 +0200

    Enhance FixturePage with mobile and desktop layouts, integrating InjuriesPreview and DisplayStandingFixture components for improved fixture details. Implement responsive tabs for mobile view to switch between standings and injuries.

---
+++ src/components/fixture/preview/injuries-preview.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+export const InjuriesPreview = () => {
+  return (
+    <Card className="w-full md:max-w-full h-fit border-2 border-redsuperlig rounded-lg p-4">
+      <CardHeader>
+        <CardTitle>Blessures</CardTitle>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
+};
